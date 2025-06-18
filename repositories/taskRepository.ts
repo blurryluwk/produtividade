8df7e605aabcef4fb6cfca7738e40eea4d12fe96
Lucas Henrique <134148040+blurryluwk@users.noreply.github.com>
2025-06-18 18:50:57 -0300

    update: taskrepository

--- repositories/taskRepository.ts
+++ repositories/taskRepository.ts
@@ -1,15 +1,29 @@
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
 export const taskRepository = {
-  create: (data: any) => prisma.task.create({ data }),
-  findByUser: (userId: number) =>
-    prisma.task.findMany({
+  create: async (data: any) => {
+    return await prisma.task.create({ data });
+  },
+
+  findByUser: async (userId: number) => {
+    return await prisma.task.findMany({
       where: { userId },
       orderBy: { createdAt: "desc" },
-    }),
-  update: (taskId: number, data: any) =>
-    prisma.task.update({ where: { id: taskId }, data }),
-  delete: (taskId: number) =>
-    prisma.task.delete({ where: { id: taskId } }),
+    });
+  },
+
+  update: async (taskId: number, data: any) => {
+    return await prisma.task.update({
+      where: { id: taskId },
+      data,
+    });
+  },
+
+  delete: async (taskId: number) => {
+    return await prisma.task.delete({
+      where: { id: taskId },
+    });
+  },
 };
